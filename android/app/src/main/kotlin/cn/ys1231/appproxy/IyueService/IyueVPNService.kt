d68998873d260f9dce29150eff93849df01c5319
iyue <ys1231@126.com>
2024-05-29 00:21:01 +0800

    feat(vpn): :tada: 可以连接vpn了,关闭会崩溃第三方库

--- android/app/src/main/kotlin/cn/ys1231/appproxy/IyueService/IyueVPNService.kt
+++ android/app/src/main/kotlin/cn/ys1231/appproxy/IyueService/IyueVPNService.kt
@@ -4,45 +4,129 @@ import android.content.Intent
 import android.net.VpnService
 import android.os.ParcelFileDescriptor
 import android.util.Log
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.net.InetSocketAddress
-import java.nio.ByteBuffer
-import java.nio.channels.DatagramChannel
+import engine.Engine
+import engine.Key
+import okhttp3.internal.closeQuietly
+import java.util.concurrent.CountDownLatch
 
 
 class IyueVPNService : VpnService() {
     private val TAG = "iyue->${this.javaClass.simpleName}"
     private var vpnInterface: ParcelFileDescriptor? = null
-    private lateinit var vpnThread: Thread
+    private var vpnThread: Thread? = null
+
+    // 移除死循环，并添加一个停止信号处理逻辑
+    private val stopSignal = CountDownLatch(1)
+
+    companion object {
+        const val ACTION_STOP_SERVICE = "cn.ys1231.appproxy.STOP_VPN_SERVICE"
+    }
 
     override fun onCreate() {
         super.onCreate()
         // 初始化操作
-        Log.d(TAG, "onCreate: ")
+        Log.d(TAG, "onCreate: IyueVPNService ")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d(TAG, "onStartCommand: ")
-        // TODO 启动 VPN 连接
-        return START_STICKY
+        if (intent?.action == ACTION_STOP_SERVICE) {
+            stopVpn()
+//            stopSelf()
+            return START_NOT_STICKY
+        }
+        val proxyData = intent?.extras?.getSerializable("data") as Map<String, Any>
+        Log.d(TAG, "onStartCommand: $proxyData")
+        vpnThread = object : Thread() {
+            override fun run() {
+                try {
+                    startVpn(proxyData)
+                } catch (e: Exception) {
+                    Log.e(TAG, "vpnThread: fail", e)
+                }
+            }
+        }
+        vpnThread?.start()
+        return START_NOT_STICKY
     }
 
-    private fun startVpn() {
-        Log.d(TAG, "startVpn: ")
+    private fun startVpn(data: Map<String, Any>) {
+
+        Log.d(TAG, "startVpn: IyueVPNService: $data")
+        // {proxyPort=8080, proxyPass=, proxyName=test, proxyType=http, proxyUser=, appProxyPackageList=[com.android.chrome], proxyHost=192.168.0.1}
         val builder = Builder()
+            .addAddress("10.0.0.2", 24)
+            .addRoute("0.0.0.0", 0)
+            .setMtu(1500)
+//            .addDnsServer("192.168.10.1")
+            .setSession(applicationContext.packageName)
+        for (appPackageName in data["appProxyPackageList"] as List<String>) {
+            try {
+                Log.d(TAG, "addAllowedApplication: $appPackageName")
+                builder.addAllowedApplication(appPackageName)
+            } catch (e: Exception) {
+                Log.e(TAG, "addAllowedApplication: ${e.message}")
+            }
+        }
+        try {
+
+            vpnInterface = builder.establish()
+            if (vpnInterface == null) {
+                Log.e(TAG, "vpnInterface: create establish error ")
+                return
+            }
+            val proxyHost = data["proxyHost"].toString()
+            val proxyPort = (data["proxyPort"] as String).toInt()
+            val proxyType = data["proxyType"].toString()
+            val proxyUser = data["proxyUser"].toString()
+            val proxyPass = data["proxyPass"].toString()
+
+            val key = Key()
+            key.mark = 0
+            key.mtu = 1500
+            key.device = "fd://" + vpnInterface!!.fd // <--- here
+            key.setInterface("")
+            key.logLevel = "debug"
+            key.proxy = "${proxyType}://${proxyHost}:${proxyPort}" // <--- and here
+            key.restAPI = ""
+            key.tcpSendBufferSize = ""
+            key.tcpReceiveBufferSize = ""
+            key.tcpModerateReceiveBuffer = false
+            Engine.insert(key)
+            Engine.start()
+            Log.d(TAG, "startEngine: ${key.toString()}")
+            stopSignal.await()
+        } catch (e: Exception) {
+            Log.e(TAG, "startEngine: error ${e.message}")
+        } finally {
+            if (vpnInterface != null) {
+                vpnInterface!!.close()
+                Engine.stop()
+                vpnInterface = null
+            }
+            Log.d(TAG, "stopEngine: success!")
+        }
 
     }
 
     private fun stopVpn() {
-        Log.d(TAG, "stopVpn: ")
-        vpnInterface?.close()
-        vpnThread.interrupt()
+        Log.d(TAG, "stopVpn: vpnInterface $vpnInterface")
+        try {
+            // 通知startVpn中的等待线程可以结束
+            stopSignal.countDown();
+            if (vpnThread != null && vpnThread?.isAlive == true) {
+                vpnThread?.interrupt()
+            } else {
+                Log.w(TAG, "vpnThread is either null or not alive, interrupt is not called.")
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "stopVpn: ${e.message}")
+        }
+
     }
 
     override fun onDestroy() {
-        Log.d(TAG, "onDestroy: ")
         super.onDestroy()
-        // 释放资源
+        Log.d(TAG, "onDestroy: ")
     }
+
 }
--- android/app/src/main/kotlin/cn/ys1231/appproxy/MainActivity.kt
+++ android/app/src/main/kotlin/cn/ys1231/appproxy/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.VpnService
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import cn.ys1231.appproxy.IyueService.IyueVPNService
 import cn.ys1231.appproxy.data.Utils
@@ -11,15 +13,19 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.BasicMessageChannel
 import io.flutter.plugin.common.MethodChannel
+import java.io.Serializable
 import java.util.Objects
 
 class MainActivity : FlutterActivity() {
     private val TAG = "iyue->${this.javaClass.simpleName}"
     private val CHANNEL = "cn.ys1231/appproxy"
     private val CHANNEL_VPN = "cn.ys1231/appproxy/vpn"
+    private var intentVPNService: Intent? = null
+    private val ACTION_STOP_SERVICE = "cn.ys1231.appproxy.STOP_VPN_SERVICE"
 
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        intentVPNService = Intent(this, IyueVPNService::class.java)
         MethodChannel(
             flutterEngine.dartExecutor.binaryMessenger,
             CHANNEL
@@ -49,7 +55,7 @@ class MainActivity : FlutterActivity() {
                 }
             }else if (call.method == "stopVpn") {
                 try {
-                    stopVpn(this)
+                    stopVpnService(this)
                     result.success(true)
                 } catch (e: Exception) {
                     result.error("-1", e.message, null)
@@ -65,7 +71,7 @@ class MainActivity : FlutterActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 // 用户授权成功，启动VPN服务
                 Log.d(TAG, "onActivityResult: 用户授权成功，启动VPN服务 ")
-                startService(Intent(this, IyueVPNService::class.java))
+                startService(intentVPNService)
             } else {
                 // 用户拒绝授权，处理相应逻辑
                 Log.d(TAG, "onActivityResult: 用户拒绝授权 ")
@@ -77,15 +83,22 @@ class MainActivity : FlutterActivity() {
     private val VPN_REQUEST_CODE = 100
     private fun startVpn(context: Context, proxy:Map<String,Any>?) {
         Log.d(TAG, "startVpn: $proxy")
+        // 传递数据
+        intentVPNService?.putExtra("data", proxy as Serializable)
+        // 准备建立 VPN 连接 检测用户是否同意
         var intent = VpnService.prepare(context)
         if (intent != null) {
             this.startActivityForResult(intent, VPN_REQUEST_CODE)
         } else {
-            context.startService(Intent(context, IyueVPNService::class.java))
+            context.startService(intentVPNService)
         }
     }
-    private fun stopVpn(context: Context) {
-
+    private fun stopVpnService(context: Context) {
+        val intent = Intent(context, IyueVPNService::class.java)
+        intent.setAction(ACTION_STOP_SERVICE)
+        context.startService(intent)
+        val result = context.stopService(intent)
+        Log.d(TAG, "stopService: state:$result")
     }
 
 }
